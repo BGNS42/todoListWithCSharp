e14f21b61ac0982c17c4d4434a1ab2a25acea3ed
BGNS <196232533+BGNS42@users.noreply.github.com>
2026-04-25 18:52:07 -0300

    Creation of GerenciadorDeTarefas class n methods Added a method to add a todo to the list and another one to show the content of the list of todos.

--- main/Program.cs
+++ main/Program.cs
@@ -4,7 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Tarefa todos = new Tarefa("Titulo da tarefa", "descricao da tarefa");
-        Console.WriteLine($"{todos.Id}");
+        // Tarefa todos = new Tarefa("Titulo da tarefa", "descricao da tarefa");
+        // Tarefa todos2 = new Tarefa("Titulo da tarefa2", "descricao da tarefa2");
+        // //Console.WriteLine($"{todos.Id}");
+        // GerenciadorDeTarefas gere = new GerenciadorDeTarefas();
+
+        // gere.AdicionarTarefa(todos);
+        // gere.AdicionarTarefa(todos2);
+        // gere.AdicionarTarefa(todos2);
+
+        // string list = gere.MostrarListaDeTarefas();
+
+        // Console.WriteLine(list);
+        Console.WriteLine("Bem vindo, o que deseja?");
+        Console.WriteLine("1 - Adicionar tarefa");
+        Console.WriteLine("2 - Listar tarefas");
+        Console.WriteLine("0 - sair");
+        string inputServico = Console.ReadLine();
+        switch (inputServico)
+        {
+            case "1":
+                Console.WriteLine("Você escolheu adicionar uma tarefa");
+                break;
+            case "2":
+                Console.WriteLine("Você escolheu listar as tarefa");
+                break;
+            case "0":
+                Console.WriteLine("Você escolheu sair");
+                break;
+            default:
+                Console.WriteLine("Digite um valor entre 0 e 2");
+                Console.ReadLine();
+                break;
+        }
     }
 }
---
+++ main/classes/GerenciadorDeTarefas.cs
@@ -0,0 +1,19 @@
+public class GerenciadorDeTarefas
+{
+    private List<Tarefa> tarefas = new List<Tarefa>();
+
+    public void AdicionarTarefa(Tarefa tarefa)
+    {
+        tarefas.Add(tarefa);
+    }
+
+    public string MostrarListaDeTarefas()
+    {
+        string todo = "";
+        foreach (var tarefa in tarefas)
+        {
+            todo += $"Titulo: {tarefa.Titulo}\nDescrição: {tarefa.Descricao}\nConcluída: {tarefa.Concluida}\n\n";
+        }
+        return todo;
+    }
+}

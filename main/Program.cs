ea6e5ba4327d721032b5b20c6970dd1085c9676e
BGNS <196232533+BGNS42@users.noreply.github.com>
2026-04-26 11:23:17 -0300

    Created the user flow in program with a while loop

--- main/Program.cs
+++ main/Program.cs
@@ -16,26 +16,36 @@ class Program
         // string list = gere.MostrarListaDeTarefas();
 
         // Console.WriteLine(list);
-        Console.WriteLine("Bem vindo, o que deseja?");
-        Console.WriteLine("1 - Adicionar tarefa");
-        Console.WriteLine("2 - Listar tarefas");
-        Console.WriteLine("0 - sair");
-        string inputServico = Console.ReadLine();
-        switch (inputServico)
+        Console.WriteLine("Digite o seu nome:");
+        string usuario = Console.ReadLine();
+        bool encerrou = false;
+        Console.WriteLine($"Bem vindo(a) {usuario}, o que deseja?");
+        while (!encerrou)
         {
-            case "1":
-                Console.WriteLine("Você escolheu adicionar uma tarefa");
-                break;
-            case "2":
-                Console.WriteLine("Você escolheu listar as tarefa");
-                break;
-            case "0":
-                Console.WriteLine("Você escolheu sair");
-                break;
-            default:
-                Console.WriteLine("Digite um valor entre 0 e 2");
-                Console.ReadLine();
-                break;
+            Console.WriteLine("1 - Adicionar tarefa");
+            Console.WriteLine("2 - Listar tarefas");
+            Console.WriteLine("0 - sair");
+            string inputServico = Console.ReadLine();
+            switch (inputServico)
+            {
+                case "1":
+                    Console.WriteLine("Você escolheu adicionar uma tarefa");
+                    break;
+                case "2":
+                    Console.WriteLine("Você escolheu listar as tarefa");
+                    break;
+                case "0":
+                    Console.WriteLine("Você escolheu sair");
+                    Console.WriteLine("Pressione ENTER para confirmar");
+                    Console.ReadLine();
+                    Console.WriteLine("Programa Encerrado.");
+                    encerrou = true;
+                    break;
+                default:
+                    Console.WriteLine("Digite um valor entre 0 e 2");
+                    Console.ReadLine();
+                    break;
+            }
         }
     }
 }
